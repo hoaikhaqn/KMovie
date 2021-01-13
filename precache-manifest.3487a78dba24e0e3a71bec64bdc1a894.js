114c6114ec5472a1eafc6ddf7a06e3cbe32f7ed3
Nguyễn Hoài Kha <khahk@s-wifi.vn>
2021-01-13 14:14:58 +0700

    Updates

---
+++ precache-manifest.3487a78dba24e0e3a71bec64bdc1a894.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b583819d33ffdd81a807374c57d35a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bddd46956ae928514810",
+    "url": "./static/css/10.370da13e.chunk.css"
+  },
+  {
+    "revision": "bc7d45eeb6dec2d43c64",
+    "url": "./static/css/11.370da13e.chunk.css"
+  },
+  {
+    "revision": "d1f1522afe23102af2d6",
+    "url": "./static/css/12.d68b5987.chunk.css"
+  },
+  {
+    "revision": "5ccf998405ab7efc9c4e",
+    "url": "./static/css/3.a1606a56.chunk.css"
+  },
+  {
+    "revision": "f87b8231161f324c4065",
+    "url": "./static/css/4.0f632a66.chunk.css"
+  },
+  {
+    "revision": "319882204d68041da6f9",
+    "url": "./static/css/5.7b8f6ce2.chunk.css"
+  },
+  {
+    "revision": "7aee7c0ef070cd473c8f",
+    "url": "./static/css/6.1cf5d15a.chunk.css"
+  },
+  {
+    "revision": "2fe0882010b3759df62a",
+    "url": "./static/css/7.32f23d7d.chunk.css"
+  },
+  {
+    "revision": "eb260f94f78387f5eb2d",
+    "url": "./static/css/8.7b8f6ce2.chunk.css"
+  },
+  {
+    "revision": "a1b49ff3ac6a976ad903",
+    "url": "./static/css/main.dcecba07.chunk.css"
+  },
+  {
+    "revision": "691dd0f63134a8876b9b",
+    "url": "./static/js/0.c228ce5d.chunk.js"
+  },
+  {
+    "revision": "bddd46956ae928514810",
+    "url": "./static/js/10.d71a148b.chunk.js"
+  },
+  {
+    "revision": "bc7d45eeb6dec2d43c64",
+    "url": "./static/js/11.b24fc0f1.chunk.js"
+  },
+  {
+    "revision": "d1f1522afe23102af2d6",
+    "url": "./static/js/12.bb9954e5.chunk.js"
+  },
+  {
+    "revision": "259cfc31f6b0c8888ddd",
+    "url": "./static/js/13.9c57a49e.chunk.js"
+  },
+  {
+    "revision": "4351ff9ab8002f98c57e",
+    "url": "./static/js/14.7efaafb9.chunk.js"
+  },
+  {
+    "revision": "e7a3410a53c9a9b31bc8",
+    "url": "./static/js/15.7d1cbced.chunk.js"
+  },
+  {
+    "revision": "214ed8ad0a5d30db3ea8",
+    "url": "./static/js/16.11835b52.chunk.js"
+  },
+  {
+    "revision": "984825321aa884870b04",
+    "url": "./static/js/17.233cc6e3.chunk.js"
+  },
+  {
+    "revision": "5ccf998405ab7efc9c4e",
+    "url": "./static/js/3.f59d4bdf.chunk.js"
+  },
+  {
+    "revision": "f87b8231161f324c4065",
+    "url": "./static/js/4.aea14116.chunk.js"
+  },
+  {
+    "revision": "319882204d68041da6f9",
+    "url": "./static/js/5.02b0b823.chunk.js"
+  },
+  {
+    "revision": "7aee7c0ef070cd473c8f",
+    "url": "./static/js/6.197f82ca.chunk.js"
+  },
+  {
+    "revision": "2fe0882010b3759df62a",
+    "url": "./static/js/7.c12d8e7a.chunk.js"
+  },
+  {
+    "revision": "eb260f94f78387f5eb2d",
+    "url": "./static/js/8.79cede6c.chunk.js"
+  },
+  {
+    "revision": "10ae5c25a1c8c212c858",
+    "url": "./static/js/9.6add07fa.chunk.js"
+  },
+  {
+    "revision": "a1b49ff3ac6a976ad903",
+    "url": "./static/js/main.c610fea3.chunk.js"
+  },
+  {
+    "revision": "5ab7d4a900c2bd1c5b94",
+    "url": "./static/js/runtime~main.ef89256d.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "./static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a7063e0c0f0cb546ad45e9e24b27bd3b",
+    "url": "./static/media/Montserrat-RegularItalic.a7063e0c.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.46b5232ab62feec6db76cca08bbb8210.js"
+  "./precache-manifest.3487a78dba24e0e3a71bec64bdc1a894.js"
 );
 
 self.addEventListener('message', (event) => {
